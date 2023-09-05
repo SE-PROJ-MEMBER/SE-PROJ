49f3f4647c993f05aa5f50ef7587f34dda8333c3
ZMYLYH <zmylyh@outlook.com>
2023-09-05 21:42:05 +0800

    逻辑改动

--- connect.py
+++ connect.py
@@ -4,7 +4,10 @@ conn = sqlite3.connect('info.db')
 cursor = conn.cursor()
 
 cursor.execute(
-    'CREATE TABLE IF NOT EXISTS room(room_num INT PRIMARY KEY,ck_in DATE,ck_out DATE,room_type TEXT,room_price INT,room_status INT)')
+    'CREATE TABLE IF NOT EXISTS room(room_num INT PRIMARY KEY,room_type TEXT,room_price INT)')
 
 cursor.execute(
-    'CREATE TABLE IF NOT EXISTS user(user_id INT PRIMARY KEY,user_name TEXT,user_phone INT,user_email TEXT,user_card INT) ')
+    'CREATE TABLE IF NOT EXISTS user(user_id INT PRIMARY KEY,user_name TEXT,user_phone INT,user_email TEXT,user_card INT,user_pwd TEXT)')
+
+cursor.execute(
+    'CREATE TABLE IF NOT EXISTS orderl(order_id INT PRIMARY KEY,room_num INT,user_id INT,ck_in DATE,ck_out DATE,order_status INT)')
