fc5b080db03ed6d0d1c7895a4f3253a9a5001053
ZMYLYH <zmylyh@outlook.com>
2023-09-06 19:44:15 +0800

    显示页面

--- QT_test2/page_1/main.py
+++ QT_test2/page_1/main.py
@@ -2,3 +2,15 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 import LOG_IN, SU, FA
 
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    win = QtWidgets.QDialog()
+    window = LOG_IN.Ui_Dialog()
+    
+    window.setupUi(win)
+    
+    win.show()
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
