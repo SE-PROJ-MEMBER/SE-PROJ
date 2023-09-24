e97f070a6036aec4e26433d1670c69b441e3ac51
sdlfksf <144025277+sdlfksf@users.noreply.github.com>
2023-09-24 13:01:49 +0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,29 +1,34 @@
 import sqlite3
 
-# 创建数据库连接
+
 conn = sqlite3.connect('info.db')
 
-# 9. 通过order_id修改订单信息
-def update_order(order_id, update_data):
+# 9. 更新订单信息
+def update_order(order_id, room_num, user_id, ck_in, ck_out, order_status, comment):
     cursor = conn.cursor()
-    keys = ', '.join(f'{k}=?' for k in update_data.keys())
-    values = tuple(update_data.values())
-    sql = f'UPDATE order SET {keys} WHERE order_id=?'
-    cursor.execute(sql, values + (order_id,))
+    cursor.execute(
+        '''
+        UPDATE orderl 
+        SET room_num=?, user_id=?, ck_in=?, ck_out=?, order_status=?, comment=? 
+        WHERE order_id=?
+        ''', 
+        (room_num, user_id, ck_in, ck_out, order_status, comment, order_id)
+    )
     conn.commit()
 
-# 10. 通过order_id进行评论
+# 10. 添加订单评论
 def comment_order(order_id, comment):
     cursor = conn.cursor()
-    cursor.execute('SELECT order_status FROM order WHERE order_id = ?', (order_id,))
+    cursor.execute('SELECT order_status FROM orderl WHERE order_id = ?', (order_id,))
     result = cursor.fetchone()
-    if result is None or result[0] != 2:
+    if result and result[0] == 2:
+        cursor.execute('UPDATE orderl SET comment = ? WHERE order_id = ?', (comment, order_id))
+        conn.commit()
+        return "Comment posted successfully."
+    else:
         return "Cannot comment on this order. Only orders with status 2 can be commented."
-    cursor.execute("UPDATE order SET comment = ? WHERE order_id = ?", (comment, order_id))
-    conn.commit()
-    return "Comment posted successfully."
 
-# 11. 返回所有用户信息
+# 11. 获取所有用户信息
 def get_all_users():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM user")
