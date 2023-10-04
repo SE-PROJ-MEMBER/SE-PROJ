12f38350db0a4158743b723c04103ef65794ae75
YUUEMO <144028840+YUUEMO@users.noreply.github.com>
2023-10-04 12:26:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,90 @@
 import backend
+import sqlite3
+# 测试9.更新订单信息
+def test_update_order():
 
-# 9. 创建一个订单
-order_id = 1
-room_num = 102
-user_id = 2
-ck_in = '2023-10-01'
-ck_out = '2023-10-05'
-order_status = 2
-comment = 'This is a comment'
-backend.update_order(order_id, room_num, user_id, ck_in, ck_out, order_status, comment)
+    #读取原订单信息
+    order_id = 1
+    conn = sqlite3.connect('info.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM orderl WHERE order_id = ?", (order_id,))
+    original_order = cur.fetchone()
+    conn.close()
+    
+    #更新订单信息
+    item_list = ['room_num','user_id', 'ck_in', 'ck_out', 'order_status', 'comment' ]
+    value_list = ['102','2','2023-10-1','2023-10-5','2','This is a comment']
+    for i in range(5):
+        backend.update_order(item_list[i], value_list[i], order_id)
 
-# 10. 尝试为订单添加评论
-new_comment = 'This is a new comment'
-comment_result = backend.comment_order(order_id, new_comment)
-print(comment_result)
+    # 读取更新后订单信息
+    connection = sqlite3.connect('info.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM orderl WHERE order_id = ?", (order_id,))
+    updated_order = cursor.fetchone()
+    connection.close()
 
-# 11. 获取所有用户信息
-users = backend.get_all_users()
-for user in users:
-    print(user)
+    #确认订单信息是否已更新
+    if updated_order is not None:
+        if updated_order == original_order:
+            print('9.订单信息未更新')
+        else:
+            print('9.订单信息已更新')
+            for i in range[6]:
+                if updated_order[i] == original_order[i]:
+                    continue
+                else:
+                    print(f'原订单信息{original_order[i]}已更新为{updated_order[i]}')
+                    continue
+    else:
+        print('9.无订单信息')
+
+# 测试10.添加订单评论
+def test_comment_order():
+    order_id = 1
+    new_comment = 'This is a new comment'
+    comment_result = backend.comment_order(order_id, new_comment)
+
+    # 读取订单评论
+    connection = sqlite3.connect('info.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM orderl WHERE order_id = ?", (order_id,))
+    order = cursor.fetchone()
+    connection.close()
+    
+    #判断订单评论是否成功修改
+    if order is not None:
+        if order[6] == new_comment:
+            print("10.添加订单评论通过")
+        else:
+            print("10.添加订单评论失败，读取订单评论与输入评论不符")
+    else: 
+        print('10.添加订单评论失败，无订单信息')
+
+# 测试11.返回用户信息
+def verify_get_all_users():
+    # 连接数据库
+    connection = sqlite3.connect('info.db')
+    cursor = connection.cursor()
+
+    # 查询所有用户信息
+    cursor.execute("SELECT * FROM user")
+    users_from_db = cursor.fetchall()
+
+    # 获取后端返回的用户信息
+    users_from_backend = backend.get_all_users()
+
+    # 验证用户信息是否一致
+    if users_from_db == users_from_backend:
+        print("11.返回所有用户信息通过")
+    else:
+        print("11.返回所有用户信息失败")
+
+    # 关闭数据库连接
+    connection.close()
+
+#运行测试函数
+
+test_update_order()
+test_comment_order()
+verify_get_all_users()
