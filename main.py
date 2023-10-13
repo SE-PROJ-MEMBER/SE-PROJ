57bedb150d630895046fb1938c593759b1bf33dd
ZMYLYH <zmylyh@outlook.com>
2023-10-13 21:58:41 +0800

    bug fixed

--- main.py
+++ main.py
@@ -341,9 +341,9 @@ def modify_user_info():
         # Main, 'Error', alter_status, QtWidgets.QMessageBox.Ok))
         UI.reason.setText(alter_status)
         turn_page(20)
-        return
-    global g_pre_page
-    g_pre_page = 11
+        global g_pre_page
+        g_pre_page = 11
+        return    
     turn_page(12)
 
 
